305c4b5561f43497dd381636fe765b375ca2ff43
beth <beth.gooding@hotmail.com>
2021-05-12 20:13:26 +0100

    Adds solution attempt to list exercise

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
 import React from 'react';
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet, FlatList } from 'react-native';
 import ColourBox from './components/ColourBox';
 
+const COLORS = [
+  { colorName: 'Base03', hexCode: '#002b36' },
+  { colorName: 'Base02', hexCode: '#073642' },
+  { colorName: 'Base01', hexCode: '#586e75' },
+  { colorName: 'Base00', hexCode: '#657b83' },
+  { colorName: 'Base0', hexCode: '#839496' },
+  { colorName: 'Base1', hexCode: '#93a1a1' },
+  { colorName: 'Base2', hexCode: '#eee8d5' },
+  { colorName: 'Base3', hexCode: '#fdf6e3' },
+  { colorName: 'Yellow', hexCode: '#b58900' },
+  { colorName: 'Orange', hexCode: '#cb4b16' },
+  { colorName: 'Red', hexCode: '#dc322f' },
+  { colorName: 'Magenta', hexCode: '#d33682' },
+  { colorName: 'Violet', hexCode: '#6c71c4' },
+  { colorName: 'Blue', hexCode: '#268bd2' },
+  { colorName: 'Cyan', hexCode: '#2aa198' },
+  { colorName: 'Green', hexCode: '#859900' },
+];
+
 const App = () => {
+  const renderItem = ({ item }) => (
+    <ColourBox colourName={item.colorName} colourHex={item.hexCode} />
+  );
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={[styles.container]}>
         <View style={styles.titleContainer}>
-          <Text style={styles.title}>
-            Here are some boxes of different colours
-          </Text>
+          <Text style={styles.title}>Solarized</Text>
         </View>
-        <ColourBox colourName="Cyan" colourHex="#2aa198" />
-        <ColourBox colourName="Blue" colourHex="#268bd2" />
-        <ColourBox colourName="Magenta" colourHex="#d33682" />
-        <ColourBox colourName="Orange" colourHex="#cb4b16" />
+        <FlatList
+          data={COLORS}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.colorName}
+        />
       </View>
     </SafeAreaView>
   );
