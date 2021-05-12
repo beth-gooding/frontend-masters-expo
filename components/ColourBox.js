81a67407b24608e35d1361a56365d394cbaf6c8e
beth <beth.gooding@hotmail.com>
2021-05-12 19:34:30 +0100

    Makes ColourBox component

---
+++ components/ColourBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ColourBox = () => {
+  return (
+    <View>
+      <Text>I am a colour box!</Text>
+    </View>
+  );
+};
+
+export default ColourBox;
